045d6f023c7e801dc3818d5169213f2daf704ba7
Darko Bogatinov <darko.bogatinov15@gmail.com>
2025-01-19 17:03:47 +0100

    Data types, JavaScript, done

---
+++ JavaScript/Data types.js
@@ -0,0 +1,10 @@
+// string    : ' ', ""
+// number    : 123, -2, 0.34        : Float, Integers
+// boolean   : true, false          : true = 1, false = 0
+// undefined : undefined            : doesn't exist, was never defined
+// null      : null                 : explicitly defining as does not exist
+// object    : { }
+// NaN       :                      : Not a Number
+console.log(typeof 1);
+console.log(typeof "str");
+console.log(typeof true);
