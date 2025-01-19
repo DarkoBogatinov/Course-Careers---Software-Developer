e4475ffa3f20ae90e5b4b15ec17c201430258900
Darko Bogatinov <darko.bogatinov15@gmail.com>
2025-01-19 21:58:21 +0100

    Variables, JavaScript, done

---
+++ JavaScript/Variables.js
@@ -0,0 +1,25 @@
+var myNumber = 1;
+console.log(myNumber);
+
+myNumber = myNumber + 2;
+console.log(myNumber);
+
+var name1 = "tim";
+var name2 = "bob";
+console.log(name1, name2);
+
+name2 = name1;
+console.log(name1, name2);
+
+name2 = "jill";
+console.log(name1, name2);
+
+const myNumber1 = 22;
+
+let myNumber2 = 22;
+myNumber2 = 3;
+
+let myNumber3;
+console.log(myNumber3);
+myNumber3 = 24;
+console.log(myNumber3);
