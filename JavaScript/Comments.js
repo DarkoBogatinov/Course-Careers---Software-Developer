da7c00b8155f0eff35d9c020cdeb5b9ccf2179b7
Darko Bogatinov <darko.bogatinov15@gmail.com>
2025-01-19 21:19:25 +0100

    Comments, JavaScript, done

---
+++ JavaScript/Comments.js
@@ -0,0 +1,14 @@
+// hello world, this is a comment!
+console.log("below the comment"); // this prints some stuff to the console
+
+/*
+This
+is
+a
+multi
+line
+comment
+
+Multipul line comment shortcut is ctrl + shift + /
+Single line comment shortcut is ctrl + /
+*/
