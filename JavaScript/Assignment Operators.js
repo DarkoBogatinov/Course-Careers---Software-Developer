69ed6aa5882b40f8ac9a1ba68f13357145817480
Darko Bogatinov <darko.bogatinov15@gmail.com>
2025-01-20 22:05:55 +0100

    Assignment Operators, JavaScript, done

---
+++ JavaScript/Assignment Operators.js
@@ -0,0 +1,22 @@
+let num = 10;
+num += 5;
+
+console.log(num);
+
+num += num;
+console.log(num);
+
+num -= 5;
+console.log(num);
+
+num *= 2;
+console.log(num);
+
+num /= 2;
+console.log(num);
+
+num %= 7;
+console.log(num);
+
+num **= 2;
+console.log(num);
