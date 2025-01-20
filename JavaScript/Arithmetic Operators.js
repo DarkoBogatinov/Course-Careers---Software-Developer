e5988b3765e36f3ee62ae76e4d97767fde36cb73
Darko Bogatinov <darko.bogatinov15@gmail.com>
2025-01-20 21:58:47 +0100

    Arithmetic Operators, JavaScript, done

---
+++ JavaScript/Arithmetic Operators.js
@@ -0,0 +1,37 @@
+/*
++
+-
+*
+/
+**
+%
+++
+--
+*/
+
+const x = 10;
+const y = 4;
+
+let z = (x + y) + 10 / 2;
+console.log(z);
+
+/*
+B : Brackets
+E : Exponents
+D : Division
+M : Multiplication
+A : Addition
+S : Subtraction
+*/
+
+z = x ** y;
+console.log(z);
+
+z = x % y;
+console.log(z);
+
+z++;
+console.log(z);
+
+const q = 10 + (x ** 2) - (4 * 7);
+console.log(q);
