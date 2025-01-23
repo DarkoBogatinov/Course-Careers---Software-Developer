84c99786ff30b188414447c204fc5b2b873cb568
Darko Bogatinov <darko.bogatinov15@gmail.com>
2025-01-23 22:15:20 +0100

    Switch Statement, JavaScript, done

---
+++ JavaScript/Switch Statement.js
@@ -0,0 +1,31 @@
+const color = prompt("Enter your fav color: ");
+
+switch (color) {
+    case "red":
+        alert("Red is pretty cool");
+        break;
+
+    case "blue":
+        alert("Blue is the sky");
+        break;
+
+    case "orange":
+        alert("Orange is my fav");
+        break;
+
+    default:
+        alert("I do NOT like the color");
+        break;
+}
+
+const age = Number(prompt("Enter your age: ");
+
+switch (age) {
+    case 10:
+        alert("you are 10");
+        break;
+
+    case 20:
+        alert("you are 20");
+        break;
+}
